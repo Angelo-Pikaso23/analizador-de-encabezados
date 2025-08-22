7d2078d332cc27882c06021ef90d3a7ef1ceaeca
Angelo-Pikaso23 <espinozagutierrezemiliano@gmail.com>
2025-08-22 03:15:28 -0600

    commit

--- index.js
+++ index.js
@@ -20,8 +20,12 @@ app.get('/', function (req, res) {
 });
 
 // tu primer endpoint de API...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  const ipaddress = req.headers['x-forwarded-for']?.split(',')[0] || req.socket.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({ ipaddress, language, software });
 });
 
 // escuchar solicitudes :)
